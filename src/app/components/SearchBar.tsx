5e12cdd740354e04bc370786a840190d4b879a00
Hassan Kashif <hassank1751@gmail.com>
2025-03-06 03:34:36 -0800

    fix: adjust positioning of SearchBar component for improved layout

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -7,31 +7,31 @@ export default function SearchForm() {
   const cities = ["Karachi", "Lahore", "Islamabad", "Rawalpindi", "Peshawar", "Quetta", "Multan", "Faisalabad"]
 
   return (
-    <div className="absolute z-0 bottom-[-30px] bg-white p-6 w-[80vw] rounded-lg shadow-lg flex flex-col md:flex-row gap-4">
+    <div className="absolute z-0 bottom-[-30px] right-[145px] bg-white p-6 w-[80vw] rounded-lg shadow-lg flex flex-col md:flex-row gap-4">
       <Select>
         <SelectTrigger className="w-full md:w-[550px] md:h-[50px]">
           <SelectValue placeholder="Select Make" />
         </SelectTrigger>
-        <SelectContent>
+        {/* <SelectContent>
           {carMakes.map((make) => (
             <SelectItem key={make} value={make.toLowerCase()}>
               {make}
             </SelectItem>
           ))}
-        </SelectContent>
+        </SelectContent> */}
       </Select>
 
       <Select>
         <SelectTrigger className="w-full md:w-[550px] md:h-[50px]">
           <SelectValue placeholder="Select City" />
         </SelectTrigger>
-        <SelectContent>
+        {/* <SelectContent>
           {cities.map((city) => (
             <SelectItem key={city} value={city.toLowerCase()}>
               {city}
             </SelectItem>
           ))}
-        </SelectContent>
+        </SelectContent> */}
       </Select>
 
       <Button className="bg-red-500 mt-1 hover:bg-red-600 text-white md:w-[150px] md:h-[40px]">Search</Button>
